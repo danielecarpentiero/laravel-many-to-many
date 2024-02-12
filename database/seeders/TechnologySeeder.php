8ab15acac21c6231a9f817e1d690648ac9a4698e
danielecarpentiero <carpentierodaniele@gmail.com>
2024-02-12 17:22:28 +0100

    feat: technology seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $technologies = ['HTML', 'CSS', 'JavaScript', 'Vue', 'Vite', 'PHP', 'Laravel', 'SQL'];
+        foreach ($technologies as $technology){
+
+            $new_technology = new Technology();
+
+            $new_technology->title = $technology;
+            $new_technology->slug = Str::of($technology)->slug('-');
+
+            $new_technology->save();
+        }
+    }
+}
