379d8a2f6eaef8c1abc3b6abfc8208dfa5deaf73
danielecarpentiero <carpentierodaniele@gmail.com>
2024-02-12 17:53:34 +0100

    feat: sync'ed controller with technologies

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Project;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Technology;
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Models\Type;
@@ -14,7 +15,7 @@ class ProjectController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         $projects = Project::all();
 
@@ -24,16 +25,20 @@ class ProjectController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
+        // get all types
         $types = Type::all();
-        return view('admin.create', compact('types'));
+        // get all technologies
+        $technologies = Technology::all();
+
+        return view('admin.create', compact('types', 'technologies'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProjectRequest $request)
+    public function store(StoreProjectRequest $request): \Illuminate\Http\RedirectResponse
     {
 
         $data = $request->validated();
@@ -41,15 +46,20 @@ class ProjectController extends Controller
         $new_project = new Project();
         $new_project->fill($data);
         $new_project->slug = Str::of($new_project->title)->slug('-');
+
         $new_project->save();
 
-        return redirect()->route('admin.projects.index')->with('message', "Project #$new_project->id created successfully");
+        if (isset($data['technologies'])){
+            $new_project->technologies()->sync($data['technologies']);
+        }
+
+        return redirect()->route('admin.projects.index')->with('messages', "Project #$new_project->id created successfully.");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Project $project)
+    public function show(Project $project): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         return view('admin.show', compact('project'));
     }
@@ -57,33 +67,43 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Project $project)
+    public function edit(Project $project): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         $types = Type::all();
-        return view('admin.edit', compact('project', 'types'));
+        $technologies = Technology::all();
+
+        return view('admin.edit', compact('project', 'types', 'technologies'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProjectRequest $request, Project $project)
+    public function update(UpdateProjectRequest $request, Project $project): \Illuminate\Http\RedirectResponse
     {
         $data = $request->validated();
 
+        $project->slug = Str::of($data['title'])->slug('-');
         $project->update($data);
-        $project->slug = Str::of($project['title'])->slug('-');
-        $project->save();
 
-        return redirect()->route('admin.projects.index');
+        if ($request->has('technologies')){
+            $project->technologies()->sync($data['technologies']);
+        } else {
+            $project->technologies()->sync([]);
+        }
+
+        return redirect()->route('admin.projects.index')->with('messages', "Project $project updated successfully.");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Project $project)
+    public function destroy(Project $project): \Illuminate\Http\RedirectResponse
     {
+        $project->technologies()->sync([]);
+        $project_id = $project['id'];
+
         $project->delete();
 
-        return redirect()->route('admin.projects.index');
+        return redirect()->route('admin.projects.index')->with('messages', "Project $project deleted successfully.");
     }
 }
